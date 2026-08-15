eb37f88687ff4f4f61065c4049626172c04ab7f3
sartapeshivkumar <sartapeshivkumar503@gmail.com>
2026-08-15 21:50:16 +0530

    Time: 9 ms (24.29%), Space: 46.5 MB (11.49%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+        Set<String> set = new HashSet<>(wordDict);
+
+        int n = s.length();
+
+        boolean[] dp = new boolean[n + 1];
+
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                String word = s.substring(j, i);
+
+                if (dp[j] && set.contains(word)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
